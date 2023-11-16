f77aa0742d3b10fd22496d29acd45b0c1d807fd5
NasirUddincm <nasircm78@gmail.com>
2023-11-16 21:21:26 +0600

    first commit

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,69 @@
+<template>
+    <section class="featured section-padding" id="section_2">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-6 col-12">
+                    <div class="profile-thumb">
+                        <div class="profile-title">
+                            <h4 class="mb-0">About Me</h4>
+                        </div>
+
+                        <div class="profile-body">
+                            <p>
+                                <span class="profile-small-title">Name</span> 
+                                <span>Nasir Uddin</span>
+                            </p>
+
+                            <p>
+                                <span class="profile-small-title">Birthday</span> 
+                                <span>Aug 12, 1988</span>
+                            </p>
+
+                            <p>
+                                <span class="profile-small-title">Phone</span> 
+                                    <span><a href="tel: 305-240-9671">01779055517</a></span>
+                            </p>
+
+                            <p>
+                                <span class="profile-small-title">Email</span> 
+                                <span><a href="mailto:engrnasirsarker@gmail.com">engrnasirsarker@gmail.com</a></span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-12 mt-5 mt-lg-0">
+                    <div class="about-thumb">
+                        <div class="row">
+                            <div class="col-lg-6 col-6 featured-border-bottom py-2">
+                                <strong class="featured-numbers">20+</strong>
+
+                                <p class="featured-text">Years of Experiences</p>
+                            </div>
+
+                            <div class="col-lg-6 col-6 featured-border-start featured-border-bottom ps-5 py-2">
+                                <strong class="featured-numbers">245</strong>
+
+                                <p class="featured-text">Happy Customers</p>
+                            </div>
+
+                            <div class="col-lg-6 col-6 pt-4">
+                                <strong class="featured-numbers">640</strong>
+
+                                <p class="featured-text">Project Finished</p>
+                            </div>
+
+                            <div class="col-lg-6 col-6 featured-border-start ps-5 pt-4">
+                                <strong class="featured-numbers">72+</strong>
+
+                                <p class="featured-text">Digital Awards</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+</template>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-12 col-12">
+                    <div class="copyright-text-wrap">
+                        <p class="mb-0">
+                            <span class="copyright-text">Copyright © 2023 <a href="#">Nasir Uddin</a> Company. All rights reserved.</span>
+                            Design: 
+                            <a rel="sponsored" href="https://aloronit.com" target="_blank">Nasir Uddin</a>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+</template>
